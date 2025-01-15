78bffc775a819b8fbe00f2b03292a860febf909e
Anish-Kumar12 <anishkumar344567@gmail.com>
2025-01-16 03:01:47 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState , useEffect} from 'react'
 import './App.css'
 import { SwitchContext } from './context/SwitchContext'
-import Header from './components/header/Header.jsx';
+import Header from './components/header/Header';
+import Nav from './components/navbar/navbar'
 
 function App() {
   const mystorage = window.localStorage
@@ -15,6 +16,7 @@ function App() {
     <SwitchContext.Provider value={{ darkMode, setDarkMode, mystorage }}>
       <div className={`main-content ${darkMode ? 'bg-dark' : 'bg-light'}`}>
         <Header/>
+        <Nav/>
       </div>
       </SwitchContext.Provider>
   )
