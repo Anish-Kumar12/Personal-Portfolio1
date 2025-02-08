4640fc8d88817779eea06940b2631195c93921e7
Anish-Kumar12 <anishkumar344567@gmail.com>
2025-02-09 03:57:41 +0530

    Updated cv

--- src/components/header/helper.jsx
+++ src/components/header/helper.jsx
@@ -4,7 +4,7 @@ const Helper = () => {
   return (
     <div className='cta'>
       <a
-        href='https://drive.google.com/file/d/1HHljZymbRoiRFRJqQBZAWJLohRTZMCQn/view?usp=sharing'
+        href='https://drive.google.com/file/d/1rfOl3V_WM7-uQAV1meUK2lF3LwWdwiwa/view?usp=sharing'
         target='_blank'
         rel='noreferrer'
         className='btn cta-icon dl'
